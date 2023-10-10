45020a2790265d7b59e114038ce5873682e954a3
snakiso <igorchagov@mail.ru>
2023-10-10 13:40:48 +0300

    add some blocks

--- src/js/swipers.js
+++ src/js/swipers.js
@@ -1,3 +1,34 @@
+let swiper6 = undefined;
+
+function initSwiperCategory() {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 700 && swiper6 == undefined) {
+        swiper6 = new Swiper('.we-do__slider', {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 30,
+
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    } else if (screenWidth >= 700 && swiper6 !== undefined) {
+        swiper6.destroy();
+        swiper6 = undefined;
+    }
+}
+
+$(document).ready(function () {
+    initSwiperCategory();
+}); //Проверка при загрузке страницы
+
+$(window).on('resize', function () {
+    initSwiperCategory();
+}); //Запуск функции при изменении размера
+
+
+
 const swiper = new Swiper('.reviews__slider', {
     // Optional parameters
     loop: true,
@@ -28,6 +59,92 @@ const swiper1 = new Swiper('.technologies__slider', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+
+});
+
+const swiper2 = new Swiper('.construction-slider', {
+    // Optional parameters
+    loop: false,
+    spaceBetween: 30,
+    slidesPerView: 1,
+
+    breakpoints: {
+        // when window width is >= 320px
+        700: {
+            slidesPerView: 'auto',
+            spaceBetween: 30
+        },
+    },
+    // If we need pagination
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+const swiper3 = new Swiper('.pages-slider', {
+    // Optional parameters
+    loop: false,
+    spaceBetween: 30,
+    slidesPerView: 1,
+
+    breakpoints: {
+        // when window width is >= 320px
+        700: {
+            slidesPerView: 'auto',
+            spaceBetween: 30
+        },
+    },
+    // If we need pagination
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+const swiper4 = new Swiper('.we-can__slider', {
+    // Optional parameters
+    loop: false,
+    spaceBetween: 30,
+    slidesPerView: 1,
+
+    breakpoints: {
+        // when window width is >= 320px
+        1000: {
+            slidesPerView: 'auto',
+            spaceBetween: 30
+        },
+    },
+    // If we need pagination
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+const swiper5 = new Swiper('.interesting__slider', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
 
 });
